ec5dcd331ef2698494b9d739feff459dd8c4d287
zkrlibin <li_bin@sino.com.cn>
2018-06-12 18:25:07 +0800

    cxf 客户端

---
+++ webservice-client-cxf/src/main/java/com/webservice/service/weather2/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://inter.sei.service.webservice.com/")
+package com.webservice.service.weather2;
--- webservice-server-cxf/src/main/java/com/webservice/service/CXFWeatherMain2.java
+++ webservice-server-cxf/src/main/java/com/webservice/service/CXFWeatherMain2.java
@@ -12,15 +12,17 @@ import com.webservice.service.sei.inter.WeatherSeiInter2;
  */
 public class CXFWeatherMain2 {
 	public static void main(String[] args) {
-		/*		创建cxfJaxWsServerFactoryBean 用于发布服务		*/
-		JaxWsServerFactoryBean jaxWsServerFactoryBean = new JaxWsServerFactoryBean();
-		/*		设置SEI接口	--接口上必须有注解webservice	*/
-		jaxWsServerFactoryBean.setServiceClass(WeatherSeiInter2.class);
-		/*		设置SEI实现类		*/
-		jaxWsServerFactoryBean.setServiceBean(new WeatherSeiImpl2());
-		/*		设置服务地址		*/
-		jaxWsServerFactoryBean.setAddress("http://127.0.0.1:12346/weather");
-		/*		开启服务		*/
-		jaxWsServerFactoryBean.create();
+//		/*		创建cxfJaxWsServerFactoryBean 用于发布服务		*/
+//		JaxWsServerFactoryBean jaxWsServerFactoryBean = new JaxWsServerFactoryBean();
+//		/*		设置SEI接口	--接口上必须有注解webservice	*/
+//		jaxWsServerFactoryBean.setServiceClass(WeatherSeiInter2.class);
+//		/*		设置SEI实现类		*/
+//		jaxWsServerFactoryBean.setServiceBean(new WeatherSeiImpl2());
+//		/*		设置服务地址		*/
+//		jaxWsServerFactoryBean.setAddress("http://127.0.0.1:12346/weather");
+//		/*		开启服务		*/
+//		jaxWsServerFactoryBean.create();
+		System.out.println(System.getProperty("java.version"));
+		System.out.println(System.getProperty("java.home"));
 	}
 }
