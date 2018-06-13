5f544d30eee42ef5bfa45054cd2b28ad6c544269
zkrlibin <li_bin@sino.com.cn>
2018-06-13 09:41:42 +0800

    修改 .gitignore 文件，删除无用的类文件

---
+++ webservice-client-cxf/src/main/java/com/webservice/service/weather1Main.java
@@ -0,0 +1,25 @@
+package com.webservice.service;
+
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+
+import com.webservice.service.weather2.WeatherSeiInter2;
+import com.webservice.service.weather2.WeatherSeiInter2Service;
+
+public class weather1Main {
+
+	public static void main(String[] args) {
+		/* 获取 JaxWsProxyFactoryBean */
+		JaxWsProxyFactoryBean JaxWsProxyFactoryBean = new JaxWsProxyFactoryBean();
+		/* 设置服务地址 */
+		JaxWsProxyFactoryBean.setAddress("http://127.0.0.1:12346/weather?wsdl");
+//		/* 设置服务接口 */
+//		JaxWsProxyFactoryBean.setServiceClass(WeatherSeiInter2.class);
+		/* 获取服务接口实例 */
+		WeatherSeiInter2 service = JaxWsProxyFactoryBean.create(WeatherSeiInter2.class);
+		/* 调用服务 */
+		String queryWeather = service.queryWeather("123");
+		/* 打印输出 */
+		System.out.println(queryWeather);
+	}
+
+}
--- webservice-client-cxf/src/main/java/com/webservice/service/weather2Main.java
+++ webservice-client-cxf/src/main/java/com/webservice/service/weather2Main.java
@@ -2,8 +2,7 @@ package com.webservice.service;
 
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 
-import com.webservice.service.weather2.WeatherSeiInter2;
-import com.webservice.service.weather2.WeatherSeiInter2Service;
+import com.webservice.service.weather1.WeatherSeiInter1;
 
 public class weather2Main {
 
@@ -11,11 +10,11 @@ public class weather2Main {
 		/* 获取 JaxWsProxyFactoryBean */
 		JaxWsProxyFactoryBean JaxWsProxyFactoryBean = new JaxWsProxyFactoryBean();
 		/* 设置服务地址 */
-		JaxWsProxyFactoryBean.setAddress("http://127.0.0.1:12346/weather?wsdl");
+		JaxWsProxyFactoryBean.setAddress("http://127.0.0.1:12345/weather?wsdl");
 //		/* 设置服务接口 */
 //		JaxWsProxyFactoryBean.setServiceClass(WeatherSeiInter2.class);
 		/* 获取服务接口实例 */
-		WeatherSeiInter2 service = JaxWsProxyFactoryBean.create(WeatherSeiInter2.class);
+		WeatherSeiInter1 service = JaxWsProxyFactoryBean.create(WeatherSeiInter1.class);
 		/* 调用服务 */
 		String queryWeather = service.queryWeather("123");
 		/* 打印输出 */
